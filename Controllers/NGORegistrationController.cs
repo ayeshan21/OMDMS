77617b059a9f78f451de4f01c7c69882fea62ee3
ayeshan21 <ayeshasiddiqua.iubat@gmail.com>
2025-03-21 23:43:24 +0600

    Designed NGO registration page and MVC

---
+++ Controllers/NGORegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Medicine_Donation.Controllers
+{
+    public class NGORegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
