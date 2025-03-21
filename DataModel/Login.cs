5608c91a8be802b90b8fb2c3ee93855b50d0e908
ayeshan21 <ayeshasiddiqua.iubat@gmail.com>
2025-03-21 23:10:54 +0600

    designed user registration page and changed in login view page and model

--- DataModel/Login.cs
+++ DataModel/Login.cs
@@ -1,20 +1,17 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Online_Medicine_Donation.DataModel
 {
     public class Login
     {
         [Key]
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [Required(ErrorMessage = "Please fill out this field")]
         [EmailAddress]
-        [StringLength(20,ErrorMessage = "E-mail must be less than or equal to 20 characters")]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Please fill out this field")]
-        [StringLength(10, ErrorMessage = "Password must be less than or equal to 10 characters")]
         public string? Password { get; set; }
 
         [Required(ErrorMessage = "Please select the role")]
